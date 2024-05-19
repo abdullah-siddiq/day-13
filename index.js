ad368b5cc50c59983151b75a9613deb57ebde80a
this is the demo learning session <msabdullah414@gmail.com>
2024-05-19 19:13:51 +0530

    task

---
+++ index.js
@@ -0,0 +1,13 @@
+function foo(){
+    var res = document.getElementById("First Name").value;
+     
+    var res1= document.getElementById("Middle Name").value;
+     
+    var res2 = document.getElementById("Last Name").value;
+     
+    var res3 = document.getElementById("Email").value;
+     
+    var res4 = document.getElementById("Phone no").value;
+ 
+     console.log(`First Nmae:${res} Middle Name:${res1} Last Name:${res2} Email:${res3} phone no:${res4}`);
+}
